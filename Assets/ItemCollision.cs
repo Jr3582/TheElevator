122111502835b2afd1c30b099f960d5abe51686d
Channacy <68759286+channacy@users.noreply.github.com>
2024-12-31 20:08:11 -0500

    add feature to climb over boxes and collect key

--- Assets/ItemCollision.cs
+++ Assets/ItemCollision.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class ItemCollision : MonoBehaviour
 {
+    [SerializeField] private GameObject item1;
     public void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Key")
         {
-            other.gameObject.SetActive(false);
+            Debug.Log("Collision with key detetced.");
+            StartCoroutine(CollectItem());
             Debug.Log("Key item collected.");
         }
+    }
+    private IEnumerator CollectItem()
+    {
+        item1.SetActive(false);
+        yield return null;
     }
 }
