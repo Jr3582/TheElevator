c1547fd8dbea0590cd1891aba5e37ba26a7253fe
Jr3582 <jr3582@drexel.edu>
2025-01-03 20:10:17 -0500

    Changed title name and so player can spawn into LVL 1

---
+++ Assets/Scripts/GameplayLvl1/EnterGameplayLvl1.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnterGameplayLvl1 : MonoBehaviour {
+    public string nextSceneName;
+    private bool isNearDoor = false;
+    public Vector3 spawnPositionInNextScene;
+
+    private void Update() {
+        if (isNearDoor && Input.GetKeyDown(KeyCode.F)) {
+            SpawnManager spawnManager = FindObjectOfType<SpawnManager>();
+            if (spawnManager != null)
+            {
+                spawnManager.SetSpawnPosition(spawnPositionInNextScene);
+            }
+            SceneManager.LoadScene(nextSceneName);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.CompareTag("Door")) {
+            isNearDoor = true;
+            Debug.Log("Press 'E' to enter the door.");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (collision.gameObject.CompareTag("Door")) {
+            isNearDoor = false;
+        }
+    }
+
+    private void LoadNextScene() {
+        SceneManager.LoadScene(nextSceneName);
+    }
+}
