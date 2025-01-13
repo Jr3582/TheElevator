7bce4f6811cc34aed8c668e4a4934840ec81de8b
Channacy <68759286+channacy@users.noreply.github.com>
2025-01-12 20:31:31 -0500

    added script for each crate

---
+++ Assets/Crate1CollisionManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Crate1CollisionManager : MonoBehaviour
+{
+    [SerializeField] private GameObject uiItem;
+    private bool playerInRange = false;
+
+    void Start()
+    {
+        uiItem.SetActive(false);
+        Debug.Log($"Crate {gameObject.name} initialized");
+    }
+
+    void Update()
+    {
+        if (playerInRange && Input.GetKeyDown(KeyCode.F))
+        {
+            Debug.Log($"F key pressed near crate: {gameObject.name}");
+            uiItem.SetActive(true);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+}
